bfabdbae7d725872943e023afab0e3ae87f39a5f
Robert Parker <rob@gloop.io>
2019-05-16 14:23:08 +0000

    Update objectql/utils.py

--- objectql/utils.py
+++ objectql/utils.py
@@ -21,6 +21,9 @@ from graphql.type.definition import GraphQLType, GraphQLInterfaceType
 # From this response in Stackoverflow
 # http://stackoverflow.com/a/19053800/1072990
 def to_camel_case(snake_str, title=False):
+    if snake_str.startswith('_'):
+        snake_str = snake_str[1:]
+
     components = snake_str.split('_')
     # We capitalize the first letter of each component except the first one
     # with the 'title' method and join them together.
