5955957b263aa43fefccb0cbd4ef8d163ed0565a
Robert Parker <rob@parob.com>
2019-12-04 00:03:11 +0000

    added decorators

--- objectql/__init__.py
+++ objectql/__init__.py
@@ -4,3 +4,10 @@ from objectql.error import ObjectQLError
 from objectql.executor import ObjectQLExecutor
 from objectql.schema import ObjectQLSchema
 from objectql.reduce import ObjectQLFilter, TagFilter
+
+from objectql.decorators import \
+    query, \
+    mutation, \
+    object, \
+    abstract, \
+    interface
