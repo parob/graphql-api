fc6a0a63eb28df2b72d8edea3a095efd5905c8d3
Robert Parker <rob@parob.com>
2023-04-03 09:21:01 +0100

    test fix

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -24,7 +24,18 @@ from graphql_api.decorators import field
 
 def available(url, method="GET"):
     try:
-        response = request(method, url, timeout=5, verify=False)
+        response = request(
+            method,
+            url,
+            timeout=5,
+            verify=False,
+            headers={
+                "accept":
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,"
+                    "image/avif,image/webp,image/apng,*/*;q=0.8,"
+                    "application/signed-exchange;v=b3;q=0.7"
+            }
+        )
     except (ConnectionError, ConnectTimeout, ReadTimeout):
         return False
 
