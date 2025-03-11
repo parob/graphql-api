2bafd728194f635e31834997a491d224ca689766
Robert Parker <rob@parob.com>
2025-03-11 13:14:22 +0000

    Update file test_graphql.py

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -43,6 +43,26 @@ def available(url, method="GET"):
 
     return False
 
+def available_v2(url, method="GET"):
+    try:
+        response = request(
+            method,
+            url,
+            timeout=5,
+            verify=False,
+            headers={
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
+                "image/avif,image/webp,image/apng,*/*;q=0.8,"
+                "application/signed-exchange;v=b3;q=0.7"
+            },
+        )
+    except (ConnectionError, ConnectTimeout, ReadTimeout):
+        return False
+
+    if response.status_code == 400 or response.status_code == 200:
+        return True
+
+    return False
 
 # noinspection PyPep8Naming,DuplicatedCode
 class TestGraphQL:
